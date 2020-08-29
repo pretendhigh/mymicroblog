f64641a9c3f7084c729136b1439e3627f6224358
pretendhigh <springkiller@163.com>
2020-08-29 17:23:48 +0800

    day 23

--- app/__init__.py
+++ app/__init__.py
@@ -51,6 +51,8 @@ def create_app(config_class=Config):
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
     if not app.debug and not app.testing:
        # if app.config['MAIL_SERVER']:
        #     auth = None
